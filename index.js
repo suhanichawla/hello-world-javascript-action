77b92588e283cd2b3f445fdcc531b557d81a6100
Suhani Chawla <suhanichawla2000@gmail.com>
2021-06-22 18:47:01 +0530

    test push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  //adding new comment
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
